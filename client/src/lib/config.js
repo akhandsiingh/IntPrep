af01aff4fa2bf9f8672ee86267df4ab751e7a7c1
Akhand Singh <119024378+akhandsiingh@users.noreply.github.com>
2025-09-29 03:39:44 +0530

    Delete client directory

--- client/src/lib/config.js
+++
@@ -1,5 +0,0 @@
-export const API_BASE =
-  process.env.REACT_APP_API_URL ||
-  (typeof window !== "undefined"
-    ? `${window.location.protocol}//${window.location.hostname}:5000`
-    : "http://localhost:5000")
